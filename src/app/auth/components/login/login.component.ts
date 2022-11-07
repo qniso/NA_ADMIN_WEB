2bec1b1a5365e9aa8071be04f4719bf474441a6b
Roman Koryuk <roman.koriuk@mustpay.tech>
2022-11-07 17:51:44 +0200

    [ADDED] error hint v0.0.0

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,5 +1,7 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth.service';
 
 @Component({
@@ -10,33 +12,41 @@ import { AuthService } from '../../../shared/services/auth.service';
 export class LoginComponent implements OnInit {
 
   authForm!: FormGroup;
+  errorHint: boolean = false;
   
-
   constructor(
     private auth: AuthService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ) { }
 
 
   ngOnInit(): void {
     this.initializeForm();
-
   }
 
   private initializeForm(): void{
     this.authForm = this.fb.group({
       login: [null, [Validators.required, Validators.pattern('^[a-zA-Z]*$')]],
-      pass: [null, [Validators.required,Validators.minLength(8)]]
+      pass: [null, [Validators.required]]
     })
   }
-
+  
   submit(){
     let login = this.authForm.controls['login'].value
-    console.log(login);
     let pass = this.authForm.controls['pass'].value
-
+    
     this.auth.getAuth(login, pass).subscribe(res => {
       console.log(res);
+      if(res.code == 402){
+        this.authForm.controls['login'].setValue(null);
+        this.authForm.controls['pass'].setValue(null);
+        this.errorHint = true;
+      }else{
+        let result = `${JSON.stringify({"login": res.login, "role": res.role})}`
+        localStorage.setItem('currentUser_NA',`${result}`)
+        this.router.navigate(['main']);
+      }
     })
   }
 }
