7cea84cb5a9a8d123e3a25f643e17fdc3d6a4dc8
Roman Koryuk <roman.koriuk@mustpay.tech>
2022-12-06 16:09:23 +0200

    [Add] transport forms

--- src/app/cars/components/car-registration/car-registration.component.ts
+++ src/app/cars/components/car-registration/car-registration.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Transport } from 'src/app/shared/models/transport.model';
+import { TransportService } from 'src/app/shared/services/transport.service';
 
 @Component({
   selector: 'app-car-registration',
@@ -12,6 +15,8 @@ export class CarRegistrationComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private transportService: TransportService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -23,37 +28,89 @@ export class CarRegistrationComponent implements OnInit {
       nomenclatureName: ["test", [Validators.required]],
       technicalPassportSeries: ["test", [Validators.required]],
       technicalPassportIssuedBy: ["test", [Validators.required]],
-      validUntil: ["test", [Validators.required]],
-      dateOfIssuance: ["test", [Validators.required]],
+      validUntil: ["14.12.2024", [Validators.required]],
+      dateOfIssuance: ["14.12.2022", [Validators.required]],
       brand:["test", [Validators.required]],
       governmentNumber: ["test", [Validators.required]],
       VINCode: ["test", [Validators.required]],
       color: ["test", [Validators.required]],
-      numberOfSeats: ["test", [Validators.required]],
-      fullWeight: ["test", [Validators.required]],
-      massWithoutLoad:["test", [Validators.required]],
+      numberOfSeats: [0, [Validators.required]],
+      fullWeight: [0, [Validators.required]],
+      massWithoutLoad:[0, [Validators.required]],
       category:["test", [Validators.required]],
-      typeOfFuel: ["test", [Validators.required]],
+      typeOfFuel: ["GAS_GASOLINE", [Validators.required]],
       bodyType: ["test", [Validators.required]],
-      engineCapacity: ["test", [Validators.required]],
-      power: ["test", [Validators.required]],
-      ecologicalStandard: ["test", [Validators.required]],
+      engineCapacity: [0, [Validators.required]],
+      power: [0, [Validators.required]],
+      ecologicalStandard: ["EURO_1", [Validators.required]],
       contractNameAndNumber: ["test", [Validators.required]],
-      dateOfTheContract: ["test", [Validators.required]],
-      contractIsFixedterm: ["test", [Validators.required]],
-      expiryDate: ["test", [Validators.required]],
-      futureUsagePlans: ["test", [Validators.required]],
-      odonometerIndicator: ["test", [Validators.required]],
-      fuelTankValue: ["test", [Validators.required]],
-      height: ["test", [Validators.required]],
-      width: ["test", [Validators.required]],
-      length: ["test", [Validators.required]],
+      dateOfTheContract: ["12.10.2022", [Validators.required]],
+      contractIsFixedterm: [true, [Validators.required]],
+      expiryDate: ["12.12.2022", [Validators.required]],
+      futureUsagePlans: ["12.12.2024", [Validators.required]],
+      odonometerIndicator: [0, [Validators.required]],
+      fuelTankValue: [0, [Validators.required]],
+      height: [0, [Validators.required]],
+      width: [0, [Validators.required]],
+      length: [0, [Validators.required]],
     })
   }
 
   submit():void{
-    console.log(true);
-    
+    const transport: Transport = {
+        transport_card: {
+            nomenclature_name: this.carRegistration.controls['nomenclatureName'].value,
+            technical_certificate: {
+                num_and_series: this.carRegistration.controls['technicalPassportSeries'].value,
+                issued_by: this.carRegistration.controls['technicalPassportIssuedBy'].value,
+                date_end: this.carRegistration.controls['validUntil'].value,
+                date_issue: this.carRegistration.controls['dateOfIssuance'].value,
+                technical_certificate_dop_info: {
+                    brand: this.carRegistration.controls['brand'].value,
+                    state_number: this.carRegistration.controls['governmentNumber'].value,
+                    VIN_code: this.carRegistration.controls['VINCode'].value,
+                    colour: this.carRegistration.controls['color'].value,
+                    date_issue: this.carRegistration.controls['dateOfIssuance'].value,
+                    seats: this.carRegistration.controls['numberOfSeats'].value,
+                    full_weight:this.carRegistration.controls['fullWeight'].value,
+                    empty_weight: this.carRegistration.controls['massWithoutLoad'].value,
+                    category: this.carRegistration.controls['category'].value,
+                    fuel: this.carRegistration.controls['typeOfFuel'].value,
+                    body_type: this.carRegistration.controls['bodyType'].value,
+                    engine_volume: this.carRegistration.controls['engineCapacity'].value,
+                    engine_power:this.carRegistration.controls['power'].value,
+                    environmental_standard: this.carRegistration.controls['ecologicalStandard'].value,
+                }
+            },
+            using_reason_info: {
+                num_and_name_contract:this.carRegistration.controls['contractNameAndNumber'].value,
+                date_start: this.carRegistration.controls['dateOfTheContract'].value,
+                is_contract_fixed_term:this.carRegistration.controls['contractIsFixedterm'].value,
+                date_end:this.carRegistration.controls['expiryDate'].value,
+                date_next_start:this.carRegistration.controls['futureUsagePlans'].value,
+            },
+            general_info:{
+                mileage:this.carRegistration.controls['odonometerIndicator'].value,
+                fuel_tank_volume:this.carRegistration.controls['fuelTankValue'].value,
+                height:this.carRegistration.controls['height'].value,
+                width:this.carRegistration.controls['width'].value,
+                length:this.carRegistration.controls['length'].value,
+            }
+        }
+    }
+    this.transportService.saveNewTransport(transport).subscribe(
+      res => {
+        if(res.error.code == 0){
+          this.router.navigate(['/main/cars/table']);
+        }
+        console.log(res.error.description);
+      }
+    );    
   }
+  
+  cancel():void{
+    this.router.navigate(['/main/cars/table']);
+  }
+
 
 }
--- src/app/shared/services/transport.service.ts
+++ src/app/shared/services/transport.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { URLS } from 'src/app/app.config';
 
 @Injectable({
@@ -16,5 +16,11 @@ export class TransportService {
 
   saveNewTransport(value: object):Observable<any>{
     return this.http.post<any>(URLS.BASE_URL + URLS.NA_API + URLS.TRANSPORT + URLS.SAVE_NEW_TRANSPORT, value)
+    .pipe(
+      tap(res=> {
+        console.log(res);
+        
+      })
+    );
   }
 }
