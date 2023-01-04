a53394861b6a1a289df40fe55dfc56b3ccf5e109
Roman Koryuk <gnom51764@gmail.com>
2023-01-04 16:35:41 +0200

    [ADD] Start car edit

--- src/app/cars/cars-routing.module.ts
+++ src/app/cars/cars-routing.module.ts
@@ -3,21 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarRegistrationComponent } from './components/car-registration/car-registration.component';
 import { CarTableComponent } from './components/car-table/car-table.component';
 
-
-
 const routes: Routes = [
   {
     path: 'table',
     component: CarTableComponent,
   },
+
   {
     path: 'car-registration',
-    component: CarRegistrationComponent
-  }
+    component: CarRegistrationComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CarsRoutingModule { }
+export class CarsRoutingModule {}
--- src/app/shared/components/car-list/car-list.component.ts
+++ src/app/shared/components/car-list/car-list.component.ts
@@ -5,24 +5,27 @@ import { TransportService } from '../../services/transport.service';
 @Component({
   selector: 'car-list',
   templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.scss']
+  styleUrls: ['./car-list.component.scss'],
 })
 export class CarListComponent implements OnInit {
-
-  displayedColumns: string[] = ['position', 'brand', 'stateNumber', 'nomenclatureName', 'transportStatus'];
+  displayedColumns: string[] = [
+    'position',
+    'brand',
+    'stateNumber',
+    'nomenclatureName',
+    'transportStatus',
+    'edit',
+  ];
   dataSource = [];
 
-  constructor(
-    private transportService: TransportService
-  ) { }
+  constructor(private transportService: TransportService) {}
 
   ngOnInit(): void {
-    this.getTransportList()
+    this.getTransportList();
   }
 
-
-  getTransportList():void{
-    this.transportService.getAllTransport().subscribe(res => {
+  getTransportList(): void {
+    this.transportService.getAllTransport().subscribe((res) => {
       this.dataSource = res.transports;
     });
   }
